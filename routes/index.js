bebf2dbc82f6a6ff1f9fb18999c579de91855920
kangyoungmin <kangym855@gmail.com>
2023-09-21 17:48:58 +0900

    second_commit

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,12 @@ router.get('/', (req, res) => {
     res.render('index', { title: "게시판 목록"});
 });
 
-router.get("/writer", (req, res)=>{
+router.get("/write", (req, res)=>{
     res.render("write", { title: "게시판 글쓰기"});
+});
+
+router.get("/view", (req, res)=>{
+    res.render("view", { title: "게시판 내용보기"});
 });
 
 module.exports = router;
