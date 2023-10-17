eef79503d61bbec070487904f3316cbd8f5c937f
kangyoungmin <kangym855@gmail.com>
2023-10-17 17:43:59 +0900

    1017_first_commit

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ router.get("/write", (req, res) => {
 
 router.post("/write", (req, res) => {
   const rs = req.body;
-  let sql = "insert into ndboard (orNum, grNum, writer, userid, userpass, title, contents) values (?,?,?,?,?,?,?)";
+  let sql = "insert into ndboard (orNum, grNum, writer, userid, userpass, title, content) values (?,?,?,?,?,?,?)";
   conn.query(sql, [
     0, 1, rs.writer, 'guest',
     rs.pass,
@@ -99,49 +99,81 @@ router.post("/edit/:num", (req, res) => {
       if (err)
         console.log(err);
       else {
-        console.log('업데이트 성공');
+        console.log('수정 성공');
       }
     });
-  res.redirect('/view/'+num);
+  res.redirect('/view/' + num);
 });
 
-router.post("/pwdlogin", (req, res) => {
-  const { num, pass, title, content } = req.body;
-  let sql = "select * from ndboard where num = ? and userpass = ?";
-  conn.query( sql, [num, pass], (err, row, fields)=> {
-    if(err) {
-       console.log(err);
-    }else{
-       if(row.length > 0) {
-        console.log(row);
-         sql = "update ndboard set ? where num = ?";
-         conn.query(sql,[{ 
-                title: title,
-                content: content  
-         }, num],
-         (err, fields)=>{
-            if(err) {
-              res.send('0');
-              console.log(err);
-            }else{
-              res.send('1');
-              console.log("수정성공"); 
-            }
-         });               
-       }else{
-          res.send('0');
-       }
+router.post("/del", (req, res) => {
+  const { delpass, delnum } = req.body;
+  let sql = "select count(*) as ct from ndboard where num = ? and userpass = ?";
+  conn.query(sql, [delnum, delpass], (err, row, fields) => {
+    if (err) {
+      console.log(err);
+      res.send('0');
+    } else {
+      if (row[0].ct > 0) {
+        sql = "delete from ndboard where num = ?";
+        conn.query(sql, delnum, (err, fields) => {
+          if (err) {
+            console.log(err);
+            res.send('0');
+          } else {
+            console.log('삭제성공');
+            res.send('1');
+          }
+        });
+      } else {
+        console.log('비밀번호 틀림' + row[0].ct);
+        res.send('0');
+      }
     }
-  });
+  })
 })
 
-router.get('/pwdlogin/:id', (req, res)=>{
-  const id = req.params.id;
-  const sql = 'delete from ndboard where id=?';
-  conn.query(sql,id, (error, rows)=>{
-      if(error) console.log(error);
-      res.redirect('/');
-  });
-});//sql delete구문
+////////////////////////////////////////////////////////////////////////////////
+
+router.get("/rewrite/:num", (req, res)=>{
+  const { num } = req.params;
+  const sql = "select num, orNum, grNum, grLayer from ndboard where num = ?";
+  conn.query(sql, num, (err, row, fields)=>{
+     if(err){
+        console.log(err);
+     }else{
+        const rs = row[0];
+        res.render('rewrite', { title:'답변글 등록', rs});
+     }
+  })
+})
+.post("/rewrite", (req, res)=>{
+  const { ornum, grnum, grlayer, writer, pass, title, content } = req.body;
+  const userid = "guest";  //나중에 회원제 만들면서 수정예정
+  //목록의 grNum 이 받은 grNum 보다 클 경우 하나씩 업데이트
+  let sql = "update ndboard set grNum = grNum + 1 where orNum = ? and grNum > ?";
+  conn.query(sql, [ornum, grnum]);
+
+  //인서트
+  sql = "insert into ndboard "+
+        "(orNum, grNum, grLayer, writer, userid, userpass, title, content)"+
+        "values (?,?,?,?,?,?,?,?)";
+  conn.query(sql, [
+        parseInt(ornum),
+        parseInt(grnum) + 1,
+        parseInt(grlayer) + 1,
+        writer,
+        userid,
+        pass,
+        title,
+        content
+  ], (err, row, fields)=>{ 
+     if(err){
+        console.log(err);
+     }else{
+        console.log(row.indertId);
+     };
+     res.redirect("/");
+  });      
+});
 
 module.exports = router;
