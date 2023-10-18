3ade94a6583b8383c56fe2d5ab18f3a558d6bda5
kangyoungmin <kangym855@gmail.com>
2023-10-18 17:27:37 +0900

    1018_paging_ing

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,40 @@ mysqlConnObj.open(conn);  //연결 출력
 
 //기본 주소 설정
 router.get('/', (req, res) => {
-  //const sql = "select num, title,writer,memoCount, date_format(wdate, '%Y-%m-%d') as wwdate from ndboard order by num desc";
-  const sql = "select * from ndboard order by orNum desc, grNum asc";
-  conn.query(sql, (err, row, fields) => {
-    if (err)
-      console.log(err);
-    else {
-      let odate;
-      for (let rs of row) {
-        rs.grLayer *= 30;
-        odate = new Date(rs.wdate);
-        rs.wdate = `${odate.getFullYear()}-${odate.getMonth() + 1}-${odate.getDate()}`;
-      }
-      console.log(row);
-      res.render('index', { title: "게시판 목록", row: row });
-    }
-  })
+  let page = 1;
+  if (req.query.page) {
+    page = parseInt(req.query.page);
+  }
+  const maxlist = 10;
+  const perPage = 5;
+  let offset = (page - 1) * maxlist;
+  let sql = "select count(*) as maxcount from ndboard";
+  conn.query(sql, (err, row) => {
+    if (err) {
+      console.error(err)
+    } else {
+      const maxcount = row[0].maxcount;
+      //전체 페이지 수 
+      const totalPage = Math.ceil(maxcount / perPage);
 
+      let limit = `limit ${offset} , ${maxlist}`;
+
+      sql = "select * from ndboard order by num asc, grnum desc " + limit;
+      conn.query(sql, (err, row, fields) => {
+        if (err)
+          console.log(err);
+        else {
+          let odate;
+          for (let rs of row) {
+            rs.grLayer *= 30;
+            odate = new Date(rs.wdate);
+            rs.wdate = `${odate.getFullYear()}-${odate.getMonth() + 1}-${odate.getDate()}`;
+          }
+          res.render('index', { title: "게시판 목록", row: row });
+        }
+      })
+    }
+  });
 });
 
 router.get("/write", (req, res) => {
@@ -134,46 +151,46 @@ router.post("/del", (req, res) => {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-router.get("/rewrite/:num", (req, res)=>{
+router.get("/rewrite/:num", (req, res) => {
   const { num } = req.params;
   const sql = "select num, orNum, grNum, grLayer from ndboard where num = ?";
-  conn.query(sql, num, (err, row, fields)=>{
-     if(err){
-        console.log(err);
-     }else{
-        const rs = row[0];
-        res.render('rewrite', { title:'답변글 등록', rs});
-     }
+  conn.query(sql, num, (err, row, fields) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const rs = row[0];
+      res.render('rewrite', { title: '답변글 등록', rs });
+    }
   })
 })
-.post("/rewrite", (req, res)=>{
-  const { ornum, grnum, grlayer, writer, pass, title, content } = req.body;
-  const userid = "guest";  //나중에 회원제 만들면서 수정예정
-  //목록의 grNum 이 받은 grNum 보다 클 경우 하나씩 업데이트
-  let sql = "update ndboard set grNum = grNum + 1 where orNum = ? and grNum > ?";
-  conn.query(sql, [ornum, grnum]);
+  .post("/rewrite", (req, res) => {
+    const { ornum, grnum, grlayer, writer, pass, title, content } = req.body;
+    const userid = "guest";  //나중에 회원제 만들면서 수정예정
+    //목록의 grNum 이 받은 grNum 보다 클 경우 하나씩 업데이트
+    let sql = "update ndboard set grNum = grNum + 1 where orNum = ? and grNum > ?";
+    conn.query(sql, [ornum, grnum]);
 
-  //인서트
-  sql = "insert into ndboard "+
-        "(orNum, grNum, grLayer, writer, userid, userpass, title, content)"+
-        "values (?,?,?,?,?,?,?,?)";
-  conn.query(sql, [
-        parseInt(ornum),
-        parseInt(grnum) + 1,
-        parseInt(grlayer) + 1,
-        writer,
-        userid,
-        pass,
-        title,
-        content
-  ], (err, row, fields)=>{ 
-     if(err){
+    //인서트
+    sql = "insert into ndboard " +
+      "(orNum, grNum, grLayer, writer, userid, userpass, title, content)" +
+      "values (?,?,?,?,?,?,?,?)";
+    conn.query(sql, [
+      parseInt(ornum),
+      parseInt(grnum) + 1,
+      parseInt(grlayer) + 1,
+      writer,
+      userid,
+      pass,
+      title,
+      content
+    ], (err, row, fields) => {
+      if (err) {
         console.log(err);
-     }else{
+      } else {
         console.log(row.indertId);
-     };
-     res.redirect("/");
-  });      
-});
+      };
+      res.redirect("/");
+    });
+  });
 
 module.exports = router;
